5f7de738a56357201bd2e5035222a0c3abe7ec55
John Merz <jmerz826@gmail.com>
2022-01-24 18:18:23 -0500

    restricted middleware built

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,5 @@
+const req = require("express/lib/request")
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,8 +8,12 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next()
+  } else {
+    next({status: 401, message: 'You shall not pass!'})
+  }
 }
 
 /*
@@ -18,7 +24,7 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
+function checkUsernameFree(req, res, next) {
 
 }
 
@@ -30,7 +36,7 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
+function checkUsernameExists(req, res, next) {
 
 }
 
@@ -42,8 +48,9 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
+function checkPasswordLength(req, res, next) {
 
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {restricted, checkUsernameFree, checkUsernameExists, checkPasswordLength}

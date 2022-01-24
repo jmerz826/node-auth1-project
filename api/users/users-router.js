36f5182d7da9d5ec6e8de0e4818b9b8174a1709f
John Merz <jmerz826@gmail.com>
2022-01-24 18:13:02 -0500

    get userById built

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -10,6 +10,14 @@ router.get("/", (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.get('/:id', (req, res, next) => {
+  User.findById(req.params.id)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(err => next(err))
+})
+
 /**
   [GET] /api/users
 

3d6327b43a3eed60303718ff76baa7980b08563c
John Merz <jmerz826@gmail.com>
2022-01-24 21:19:11 -0500

    checking password length

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -61,7 +61,13 @@ function checkUsernameExists(req, res, next) {}
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength(req, res, next) {}
+function checkPasswordLength(req, res, next) {
+  if (req.body.password === undefined || req.body.password.length <= 3) {
+    res.status(422).json({message: 'Password must be longer than 3 chars'})
+  } else {
+    next()
+  }
+}
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
